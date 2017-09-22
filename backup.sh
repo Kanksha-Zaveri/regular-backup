ea2a4a89c8cb3e841c5853e07c9006f8b8f95386
Kanksha-Zaveri <32203941+Kanksha-Zaveri@users.noreply.github.com>
2017-09-22 22:54:41 +0530

    Add files via upload

---
+++ backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+backupFrom="/home/kanksha"
+mkdir backup
+done="yay"
+backupTo="/home/kanksha/backup"
+while [ true ]; do
+	cd $backupTo
+	tar -czvf homebackup-$(date +"%Y%m%d_%H%M%S").tar.gz $backupFrom --exclude=$backupTo 
+	echo $done
+	cd ..
+	sleep 180
+done
